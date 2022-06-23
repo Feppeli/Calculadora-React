d644dc12744e2d280424a30a12e304b19c2a667e
Feppeli <dfsilvat@gmail.com>
2022-06-23 11:56:21 -0300

    fix bugs

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -75,8 +75,8 @@ export default class Calculator extends Component {
                 <Button label="3" click={this.addDigit} />
                 <Button label="+" click={this.setOperation} operation />
                 <Button label="0" click={this.addDigit} double />
-                <Button label="." click={this.setOperation} />
-                <Button label="=" click={this.etOperation} operation />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation />
             </div>
 
         )
